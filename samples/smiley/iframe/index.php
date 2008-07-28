f2ffc16077f1ab577a3e7a89856bf4674d4b9f4e
(no author) <(no author)@41a61cd8-c433-0410-bb1c-e256eeef9e11>
2008-07-28 00:27:05 +0000

    r2365@opsdev009 (orig r112695):  srush | 2008-07-27 17:26:28 -0700  Some fixes to smooth out the mysmiley iframe approach.

--- samples/smiley/iframe/index.php
+++ samples/smiley/iframe/index.php
@@ -20,8 +20,11 @@ Here is the button for adding an info box.
 <div id="addInfo"></div>
 <br>
 Here is a link that will show a feed dialog. In a real application, you would want to run this js after a user action.
-
+<br>
 <a href="#" onclick="showFeed()">Feed Dialog</a>
+
+<div id="thanks">
+</div>
 
 <script type="text/javascript">
     function showFeed() {
@@ -30,14 +33,14 @@ Here is a link that will show a feed dialog. In a real application, you would wa
       var template_data = {'images' : [{'href':'http://www.facebook.com' , 'src' : '<?= IMAGE_LOCATION ?>smile0.jpg'}], 'mood': 'Happy', 'emote': ':)','mood_src': '<?= IMAGE_LOCATION ?>smile0.jpg'};
 
       // This the call to actually show the feed dialog
-      FB.Integration.showFeedDialog(<?php echo FEED_STORY_1;?>, template_data, [], '',
-                                   function(){alert("You just saw the feed dialog.")});
+    FB.Integration.showFeedDialog(<?php echo FEED_STORY_1;?>, template_data, [], '', null, false,
+                                   function(){document.getElementById('thanks').innerHTML = 'Thanks for looking at the feed dialog.';});
     }
 
     window.onload = function() {
     FB_RequireFeatures(["Integration"], function() {
         // Change the first arg to your api key, and the second to the path to your receiver.html file.
-        // You can copy my receiver.html to your server and it will work fine.
+        // You can copy my receiver.html from http://fbplatform.mancrushonmcslee.com/smiley/iframe/receiver.html to your server and it will work fine.
         FB.Facebook.init('<?php echo API_KEY;?>','receiver.html', null);
 
         // These two lines add a "add to profile" button under the given id.
