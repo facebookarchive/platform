10821f41da476417b832790210cf388d99511311
(no author) <(no author)@41a61cd8-c433-0410-bb1c-e256eeef9e11>
2008-09-06 22:00:24 +0000

    r2443@opsdev009 (orig r120285):  srush | 2008-09-06 14:56:15 -0700  smiley db fixes

--- samples/footprints/config.php
+++ samples/footprints/config.php
@@ -1,9 +1,28 @@
 <?php
 
-include_once $_SERVER['PHP_ROOT'].'/password.php';
+// Get these from http://developers.facebook.com
+$api_key = 'YOUR_API_KEY';
+$secret  = 'YOUR_SECRET';
+/* While you're there, you'll also want to set up your callback url to the url
+ * of the directory that contains Footprints' index.php, and you can set the
+ * framed page URL to whatever you want.  You should also swap the references
+ * in the code from http://apps.facebook.com/footprints/ to your framed page URL. */
 
-$db_ip = '10.1.200.128';
+// The IP address of your database
+$db_ip = '10.0.0.0';           
+
 $db_user = 'root';
-$db_name = 'ari';
-$api_key = 'aef90d6491c8a0ada5b68a6c5aebb8fa';
-$secret  = 'bf00b8c12ebbde3b6ea018b56b9d82a3';
+$db_pass = 'YOUR_DB_PASSWORD';
+
+// the name of the database that you create for footprints.
+$db_name = 'footprints';
+
+/* create this table on the database:
+CREATE TABLE `footprints` (
+  `from` int(11) NOT NULL default '0',
+  `to` int(11) NOT NULL default '0',
+  `time` int(11) NOT NULL default '0',
+  KEY `from` (`from`),
+  KEY `to` (`to`)
+)
+*/
